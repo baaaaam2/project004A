e12f088c7ba7468d06e178070ca4e630672981b0
baaaaam2 <rudrus1913@gmail.com>
2025-05-19 15:49:37 +0900

    Update BattleSystem.cpp
    std:: 제거

--- source/BattleSystem.cpp
+++ source/BattleSystem.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <windows.h>
+
+using namespace std;
+
 //배틀시스템
 void BattleSystem::fight(Player& player, Boss& boss) {
     system("cls");
@@ -10,22 +13,22 @@ void BattleSystem::fight(Player& player, Boss& boss) {
 
     while (fighting) {
         // 상단 상태 출력
-        std::cout << "교수 : 이 문제도 풀어보시지!\n";
+        cout << "교수 : 이 문제도 풀어보시지!\n";
         printStatus(player, boss);
 
         // 행동 선택
-        std::cout << "1. 문제 풀기\n";
-        std::cout << "2. 아이템 사용 (미구현)\n선택: ";
-        std::cin >> choice;
+        cout << "1. 문제 풀기\n";
+        cout << "2. 아이템 사용 (미구현)\n선택: ";
+        cin >> choice;
 
         if (choice == '1') {
             char atkChoice;
-            std::cout << "\n어떤 공격을 사용할까요?\n";
-            std::cout << "1. 풀기 (30 데미지, 70% 성공)\n";
-            std::cout << "2. 찍기 (50 데미지, 50% 성공)\n";
-            std::cout << "3. 컨닝하기 (70 데미지, 30% 성공)\n";
-            std::cout << "선택: ";
-            std::cin >> atkChoice;
+            cout << "\n어떤 공격을 사용할까요?\n";
+            cout << "1. 풀기 (30 데미지, 70% 성공)\n";
+            cout << "2. 찍기 (50 데미지, 50% 성공)\n";
+            cout << "3. 컨닝하기 (70 데미지, 30% 성공)\n";
+            cout << "선택: ";
+            cin >> atkChoice;
 
             int damage = 0, successRate = 0;
             switch (atkChoice) {
@@ -40,9 +43,9 @@ void BattleSystem::fight(Player& player, Boss& boss) {
             attack(player, boss, damage, successRate, "주인공", "교수님");
 
             if (boss.isDead()) {
-                std::cout << "====================\n";
-                std::cout << "시험지를 다 풀었습니다!\n";
-                std::cout << "====================\n";
+                cout << "====================\n";
+                cout << "시험지를 다 풀었습니다!\n";
+                cout << "====================\n";
                 fighting = false;
                 continue;
             }
@@ -51,14 +54,14 @@ void BattleSystem::fight(Player& player, Boss& boss) {
 
             if (player.isDead()) {
                 system("cls");
-                std::cout << "====================\n";
-                std::cout << "문제를 다 풀지 못했습니다! 게임 오버!\n";
-                std::cout << "====================\n";
+                cout << "====================\n";
+                cout << "문제를 다 풀지 못했습니다! 게임 오버!\n";
+                cout << "====================\n";
                 fighting = false;
             }
         }
         else if(choice == '2'){
-            std::cout << "아이템 기능은 아직 구현되지 않았습니다.\n";
+            cout << "아이템 기능은 아직 구현되지 않았습니다.\n";
             Sleep(1500); //1.5초 정지
         }
 
@@ -74,10 +77,10 @@ void BattleSystem::attack(Character& attacker, Character& defender, int damage, 
     std::cout << attackerName << "의 공격!\n";
     if (isAttackSuccessful(successRate)) {
         defender.takeDamage(damage);
-        std::cout << "공격 성공! " << defenderName << "에게 " << damage << " 피해!\n";
+        cout << "공격 성공! " << defenderName << "에게 " << damage << " 피해!\n";
     }
     else {
-        std::cout << "공격 실패!\n";
+        cout << "공격 실패!\n";
     }
     Sleep(1500); //1.5초 정지
 }
@@ -87,8 +90,8 @@ bool BattleSystem::isAttackSuccessful(int successRate) {
 }
 
 void BattleSystem::printStatus(const Player& player, const Boss& boss) {
-    std::cout << "====================\n";
-    std::cout << "플레이어 : " << player.hp << " / " << player.maxHP << " HP\n";
-    std::cout << "교수님 : " << boss.hp << " / " << boss.maxHP << " HP\n";
-    std::cout << "====================\n\n";
+    cout << "====================\n";
+    cout << "플레이어 : " << player.hp << " / " << player.maxHP << " HP\n";
+    cout << "교수님 : " << boss.hp << " / " << boss.maxHP << " HP\n";
+    cout << "====================\n\n";
 }
