41ca01cef6d63c3756f0c780c63f9150f2455fc2
baaaaam2 <rudrus1913@gmail.com>
2025-05-21 18:45:40 +0900

    엔딩 및 스토리 추가

--- source/BattleSystem.h
+++ source/BattleSystem.h
@@ -3,13 +3,17 @@
 
 #include "Player.h"
 #include "Boss.h"
+#include <string>
+
 //배틀시스템 클래스
+using namespace std;
+
 class BattleSystem {
 public:
-    void fight(Player& player, Boss& boss);
+    void fight(Player& player, Boss& boss, char& grade);// 수정: grade 추가
 
 private:
-    void attack(Character& attacker, Character& defender, int damage, int successRate, const std::string& attackerName, const std::string& defenderName);
+    void attack(Character& attacker, Character& defender, int damage, int successRate, const string& attackerName, const string& defenderName);
     bool isAttackSuccessful(int successRate);
     void printStatus(const Player& player, const Boss& boss);
 };
